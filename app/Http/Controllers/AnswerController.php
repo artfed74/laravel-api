141e45c9ba80e20cb896b6d9cfd9ca107aeae158
= <=>
2025-03-06 10:58:06 +0500

    fixm

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -15,15 +15,6 @@ class AnswerController extends Controller
     {
         $user = Auth::user();
         $task = Task::find($taskId);
-        $maxAttempts = 3; // Максимальное количество попыток
-
-        // Получаем количество использованных попыток из сессии или базы данных
-        $attemptsKey = "attempts_task_{$taskId}_user_{$user->id}";
-        $attempts = Cache::get($attemptsKey, 0);
-
-        if ($attempts >= $maxAttempts) {
-            return response()->json(['correct' => false, 'message' => 'Ты исчерпал все попытки!'], 400);
-        }
 
         // Проверяем, совпадает ли решение пользователя с правильным решением (без учета регистра и пробелов)
         if (trim(mb_strtolower($request->input('solution'))) === trim(mb_strtolower($task->solution))) {
@@ -43,9 +34,6 @@ class AnswerController extends Controller
             $user->group->increment('total_score', $scoreToAdd); // Обновление общего счёта пользователя
             $user->save();
 
-            // Очищаем попытки после правильного ответа
-            Cache::forget($attemptsKey);
-
             // Вызываем событие
             event(new ScoreUpdated($user, $user->group));
 
@@ -55,16 +43,12 @@ class AnswerController extends Controller
                 'message' => 'Ты заработал свой честный балл!'
             ]);
         } else {
-            // Увеличиваем количество попыток
-            $attempts++;
-            Cache::put($attemptsKey, $attempts, now()->addMinutes(30)); // Срок хранения можно изменить
-
             $user->increment('score_incorrect_task', 1);
             $user->save();
 
             return response()->json([
                 'correct' => false,
-                'message' => "Ну неет, подумай побольше! Осталось попыток: " . ($maxAttempts - $attempts)
+                'message' => "Ну неет, подумай побольше! "
             ]);
         }
     }
